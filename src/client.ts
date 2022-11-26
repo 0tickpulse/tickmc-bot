e4d0bce885cd440dd2c239060faadb45a071f6e7
0TickPulse <0tickpulse@gmail.com>
2022-11-26 23:17:44 +0800

    step 1 in migrating to yaml

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,52 @@
 import * as discord from "discord.js";
-import { token } from "src/config/token.json";
+import * as fs from "fs";
+import * as path from "path";
+import * as files from "src/util/files.js";
+import * as json from "jsonc-parser";
+
+// Config setup
+const token = json.parse(fs.readFileSync(path.join(__dirname, "config", "token.yml"), "utf8"))["token"];
+
+/**
+ * Re-scans the configuration file. **Should never be used.**
+ */
+const scanConfig = () => {
+    try {
+        return json.parse(fs.readFileSync(path.join(__dirname, "config", "config.yml"), "utf8"));
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+let config = scanConfig();
+
+/**
+ * Re-scans the configuration file and returns it.
+ */
+export const reloadConfig = () => {
+    config = scanConfig();
+    return config;
+};
+/**
+ * Gets the configuration file.
+ */
+export const getConfig = () => {
+    return config;
+};
+
+// Debuggers
+
+// export const echoDebug = (message: string): void => {
+//     if (getConfig()["debug"]) {
+//         console.log(message);
+//     }
+// };
+
+// export const echoError = (error: Error): void => {
+//     console.log(`An error has occured: ${error.message}`);
+// }
+
+// Client setup
 
 const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
 client.login(token);
@@ -9,8 +56,10 @@ client.login(token);
  */
 export const getClient = () => client;
 
+// Event setup
+
 /**
- * Registers an event handler for the bot.
+ * Registers an event handler for the bot.w
  * @param {discord.Event} event The event to register the handler for.
  * @param {Function} handler The function to be called when the event is triggered.
  * @param {boolean} once Whether the handler should be called only once.
@@ -22,3 +71,9 @@ export const registerEvent = (event: keyof discord.ClientEvents, handler: () => 
     }
     client.on(event, handler);
 };
+
+files.deepGetFiles("src/events").then((files: string[]) => {
+    files.forEach((file: string) => {
+        require(file);
+    });
+});
